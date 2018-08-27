c5044ad0907f7891a5638d048b359ea57d1fc92f
itaital <itaital100@gmail.com>
2018-08-27 16:22:46 +0300

    Finish copy button and added all the functionality of the button (Copy to clipboard and msg that the txt copy)

--- app/src/main/java/itaital100/gmail/com/terutson/DetailsActivity.java
+++ app/src/main/java/itaital100/gmail/com/terutson/DetailsActivity.java
@@ -1,64 +1,64 @@
 package itaital100.gmail.com.terutson;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import itaital100.gmail.com.terutson.ExusesFactory.Category;
+import android.content.ClipboardManager;
+import android.widget.Toast;
 
 
-public class DetailsActivity extends AppCompatActivity
-{
-    //Vars:
-            Category ActivityCategory;
-            int currentExcuseIndex;
-    //Components:
-            TextView tv;
-
+public class DetailsActivity extends AppCompatActivity {
+    TextView tv;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        tv = (TextView) findViewById(R.id.txt_teruson);
-        ActivityCategory = getExcuseCategoryType();
-        int i;
-    }
-    //-------------------------------------------------------------------------------------------
-    /*
-        Checks if this activity got any messege about the type of the category that it is meant for.
-        Output: if messege is valid -> returns the category type
-                else                -> returns null;
 
+         tv = (TextView) findViewById(R.id.txt_teruson);
+        Button copy = (Button) findViewById(R.id.btn_copy);
+
+        //----------------------------------------------------------------------------------------------
+    /*
+       when press copy it will copy the text to the clipboard
+       and it will show a msg that the text was copied.
      */
-    private Category getExcuseCategoryType()
-    {
-        if(getIntent().hasExtra("com.gmail.itaital100.hw"))
+
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("text label", tv.getText());
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(getApplicationContext(),"Text pasted",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if(getIntent().hasExtra("com.gmail.itaital100.HW"))
         {
-            return Category.Homework;
+            String text = getIntent().getExtras().getString("com.gmail.itaital100.HW");
+            tv.setText(text);
         }
         if(getIntent().hasExtra("com.gmail.itaital100.meeting"))
         {
-            return Category.Meeting;
+            String text = getIntent().getExtras().getString("com.gmail.itaital100.meeting");
+            tv.setText(text);
         }
+
         if(getIntent().hasExtra("com.gmail.itaital100.ex"))
         {
-
-            return Category.Ex;
+            String text = getIntent().getExtras().getString("com.gmail.itaital100.ex");
+            tv.setText(text);
         }
+
         if(getIntent().hasExtra("com.gmail.itaital100.work"))
         {
-            return Category.Work;
+            String text = getIntent().getExtras().getString("com.gmail.itaital100.work");
+            tv.setText(text);
         }
-        if(getIntent().hasExtra("com.gmail.itaital100.occasion"))
-        {
-            return Category.Occassion;
-        }
-        if(getIntent().hasExtra("com.gmail.itaital100.date"))
-        {
-            return Category.Date;
-        }
-        return null;
     }
-
-
 }
