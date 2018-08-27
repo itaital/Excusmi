84affe6654f0375c6e637bd88702bed1e7ff8296
Ariel Berkovich <Arielberkovich1@gmail.com>
2018-08-27 10:56:53 +0300

    Refactored the code to simplify it

--- app/src/main/java/itaital100/gmail/com/terutson/MainActivity.java
+++ app/src/main/java/itaital100/gmail/com/terutson/MainActivity.java
@@ -7,54 +7,72 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    //Categories buttons:
+            ImageButton categoryButton_hw;
+            ImageButton categoryButton_meeting;
+            ImageButton categoryButton_ex;
+            ImageButton categoryButton_work;
+    //An array to contain all categories:
+    ArrayList<ImageButton> allCategoriesButtons = new ArrayList<ImageButton>();
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initCategoriesButtons();
 
-        ImageButton hw = (ImageButton)findViewById(R.id.btn_hw);
-        ImageButton meeting = (ImageButton)findViewById(R.id.btn_meeting);
-        ImageButton ex = (ImageButton)findViewById(R.id.btn_ex);
-        ImageButton work = (ImageButton)findViewById(R.id.btn_work);
+    }
+    //----------------------------------------------------------------------------------------------
+    /*
+       This function inits all categories buttons according to their id
+       and attach a clickEvenet Listener to each one of them in a uniform way.
+     */
+    private void initCategoriesButtons()
+    {
+        //InitCategories:
+        setCategories();
+        addAllButtonsToArray(allCategoriesButtons);
 
-        //setting  listeners
-        hw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), DetailsActivity.class);
-                startIntent.putExtra("com.gmail.itaital100.HW", "תירוץ כלשהו של שיעורי בית");
-                startActivity(startIntent);
-            }
-        });
 
-        meeting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), DetailsActivity.class);
-                startIntent.putExtra("com.gmail.itaital100.meeting", "תירוץ כלשהו על פגישה");
-                startActivity(startIntent);
-            }
-        });
+        for(final ImageButton categoryButton :allCategoriesButtons)
+        {
+            categoryButton.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent startIntent = new Intent(getApplicationContext(), DetailsActivity.class);
 
-        ex.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), DetailsActivity.class);
-                startIntent.putExtra("com.gmail.itaital100.ex", "תירוץ כלשהו על מטלה");
-                startActivity(startIntent);
-            }
-        });
+                            //*******************************************************************
+                            //Notice this area:
+                            //******   This is broken on purpose, the implementation of details activity need to be changed a bit
+                            startIntent.putExtra(Integer.toString(categoryButton.getId()),categoryButton.getId());
+                            //*******************************************************************
 
-        work.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), DetailsActivity.class);
-                startIntent.putExtra("com.gmail.itaital100.work", "תירוץ כלשהו על עבודה");
-                startActivity(startIntent);
-            }
-        });
-
+                            startActivity(startIntent);
+                        }
+                    });
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    private void setCategories()
+    {
+        categoryButton_hw = (ImageButton)findViewById(R.id.btn_hw);
+        categoryButton_meeting = (ImageButton)findViewById(R.id.btn_meeting);
+        categoryButton_ex = (ImageButton)findViewById(R.id.btn_ex);
+        categoryButton_work = (ImageButton)findViewById(R.id.btn_work);
+    }
+    //----------------------------------------------------------------------------------------------
+    private void addAllButtonsToArray(ArrayList<ImageButton> arr)
+    {
+        arr.add(categoryButton_work);
+        arr.add(categoryButton_hw);
+        arr.add(categoryButton_meeting);
+        arr.add(categoryButton_ex);
     }
 }
