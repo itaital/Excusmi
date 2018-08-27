6672863aef27ddb20da98dfb8d133bff09e3ce3c
Ariel Berkovich <Arielberkovich1@gmail.com>
2018-08-27 13:32:20 +0300

    Added option to easily get the category type

--- app/src/main/java/itaital100/gmail/com/terutson/DetailsActivity.java
+++ app/src/main/java/itaital100/gmail/com/terutson/DetailsActivity.java
@@ -9,24 +9,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.content.ClipboardManager;
 import android.widget.Toast;
+import itaital100.gmail.com.terutson.ExusesFactory.Category;
 
 
-public class DetailsActivity extends AppCompatActivity {
-    TextView tv;
+public class DetailsActivity extends AppCompatActivity
+{
+    //Vars:
+        Category ActivityCategory;
+        int currentExcuseIndex;
+    //Components:
+        TextView tv;
+        Button copy;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-         tv = (TextView) findViewById(R.id.txt_teruson);
-        Button copy = (Button) findViewById(R.id.btn_copy);
-
-        //----------------------------------------------------------------------------------------------
-    /*
-       when press copy it will copy the text to the clipboard
-       and it will show a msg that the text was copied.
-     */
-
+        tv = (TextView) findViewById(R.id.txt_teruson);
+        ActivityCategory = getExcuseCategoryType();
+        initCopyButton();
+    }
+//-----------------------------------------------------------------------
+      /*
+           when press copy it will copy the text to the clipboard
+           and it will show a msg that the text was copied.
+         */
+    private void initCopyButton()
+    {
+        copy = (Button) findViewById(R.id.btn_copy);
         copy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,28 +48,42 @@ public class DetailsActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Text pasted",Toast.LENGTH_SHORT).show();
             }
         });
-
-        if(getIntent().hasExtra("com.gmail.itaital100.HW"))
+    }
+//-------------------------------------------------------------------------------------------
+    /*
+        Checks if this activity got any messege about the type of the category that it is meant for.
+        Output: if messege is valid -> returns the category type
+                else                -> returns null;
+     */
+    private Category getExcuseCategoryType()
+    {
+        if(getIntent().hasExtra("com.gmail.itaital100.hw"))
         {
-            String text = getIntent().getExtras().getString("com.gmail.itaital100.HW");
-            tv.setText(text);
+            return Category.Homework;
         }
         if(getIntent().hasExtra("com.gmail.itaital100.meeting"))
         {
-            String text = getIntent().getExtras().getString("com.gmail.itaital100.meeting");
-            tv.setText(text);
+            return Category.Meeting;
         }
-
         if(getIntent().hasExtra("com.gmail.itaital100.ex"))
         {
-            String text = getIntent().getExtras().getString("com.gmail.itaital100.ex");
-            tv.setText(text);
-        }
 
+            return Category.Ex;
+        }
         if(getIntent().hasExtra("com.gmail.itaital100.work"))
         {
-            String text = getIntent().getExtras().getString("com.gmail.itaital100.work");
-            tv.setText(text);
+            return Category.Work;
         }
+        if(getIntent().hasExtra("com.gmail.itaital100.occasion"))
+        {
+            return Category.Occassion;
+        }
+        if(getIntent().hasExtra("com.gmail.itaital100.date"))
+        {
+            return Category.Date;
+        }
+        return null;
     }
+//-------------------------------------------------------------------------------------------
+
 }
