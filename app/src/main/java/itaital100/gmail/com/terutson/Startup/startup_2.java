b2a4f981bd27cece14377e2a19b962496c41a2e8
Ariel Berkovich <Arielberkovich1@gmail.com>
2018-10-24 15:40:53 +0300

    Background and Forwards now have text inside of them

--- app/src/main/java/itaital100/gmail/com/terutson/Startup/startup_2.java
+++ app/src/main/java/itaital100/gmail/com/terutson/Startup/startup_2.java
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import itaital100.gmail.com.terutson.R;
@@ -21,22 +24,18 @@ public class startup_2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_2);
         Button nextButton = findViewById(R.id.next_button2);
-        final RadioButton agreeButton = findViewById(R.id.agreeBtn);
+        final CheckBox agreeButton = findViewById(R.id.agreeBtn);
         final AppCompatActivity this_start2_Activity = this;
-        final TextView thanksText =findViewById(R.id.textView2);
-        Utils.setGraphicsRegionTo("en",this);
+        final TextView thanksText = findViewById(R.id.textView2);
+        Utils.setGraphicsRegionTo("en", this);
         thanksText.setShadowLayer(2, 0, 0, Color.BLACK);
-
-        nextButton.setOnClickListener(   new View.OnClickListener() {
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!agreeButton.isChecked())
-                {
-                    Utils.openConfirmDialog("יש ללחוץ על כפתור ההסכמה בכדי להמשיך","אוקיי",this_start2_Activity);
-                }
-                else
-                {
-                     Intent startIntent = new Intent(getApplicationContext(), startup_3.class);
+                if (!agreeButton.isChecked()) {
+                    Utils.openConfirmDialog("יש ללחוץ על כפתור ההסכמה בכדי להמשיך", "אוקיי", this_start2_Activity);
+                } else {
+                    Intent startIntent = new Intent(getApplicationContext(), startup_3.class);
                     //start activity://
                     startActivity(startIntent);
                 }
@@ -45,8 +44,11 @@ public class startup_2 extends AppCompatActivity {
         });
 
     }
+
     @Override
     public void onBackPressed() {
         this.moveTaskToBack(true);
     }
+
+
 }
